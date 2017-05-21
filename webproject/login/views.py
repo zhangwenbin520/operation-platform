977c369ed498aa14f307aecd94b4ccc7c9f8997f
root <root@zhangwenbin.(none)>
2017-05-22 06:13:21 +0800

    webproject

--- webproject/login/views.py
+++ webproject/login/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render_to_response
 from django.contrib import auth
 from django.http import HttpResponse,HttpResponseRedirect
 
-# Create your views here.
+## Create your views here.
 
 def index(request):
     '''判断用户是否登陆'''
